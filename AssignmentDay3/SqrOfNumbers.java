08ce9cfe151631c524a3b8eb4bb3b8394d997567
ShaShubham <112076078+ShaShubham@users.noreply.github.com>
2022-09-05 17:23:51 +0530

    Update SqrOfNumbers.java

--- AssignmentDay3/SqrOfNumbers.java
+++ AssignmentDay3/SqrOfNumbers.java
@@ -8,8 +8,8 @@ public class SqrOfNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
-		List<Integer> SquareOfNum=numbers.stream().distinct().map(x -> x*x).collect(Collectors.toList());
-		System.out.println(SquareOfNum);
+		List<Integer> SquareOfNumList=numbers.stream().distinct().map(x -> x*x).collect(Collectors.toList());
+		System.out.println(SquareOfNumList);
 
 	}
 
